b5ff771bfdaff9abc5bb3866945decf2e59470b0
VatsalSharma77 <vatsalsharma0217@gmail.com>
2024-05-09 11:23:04 +0530

    backend

--- backend/app.js
+++ backend/app.js
@@ -2,17 +2,20 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const connectDB = require("./db/connect");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 5000;
 const user_routes = require("./routes/users");
 const products_routes = require("./routes/products");
 const errorHandler = require("./middlewares/errorHandler");
 
+
 app.get("/", (req, res) => {
   res.send("hello world");
 });
 
 app.use(express.json());
+app.use(cors());
 app.use("/api/users", user_routes);
 app.use("/api/products", products_routes);
 
